fd5aac093916c05ce16af5f682657767221668d2
ronny <ronny@DESKTOP-R0RPKSA>
2023-01-29 13:00:09 -0500

    Métodos set y get

--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -134,12 +134,271 @@ public class Usuario {
     }
     
     public Usuario(Usuario user1){
-        //nombre = user1.getNombre();
+        nombre = user1.getNombre();
     }
     
     /**
      * Métodos Set
      */
+    
+    /**
+    * Lee el nombre del usuario
+    * @return nombre del usuario
+    */
+    public String getNombre() {
+//        nombre = "Juanito";
+        return nombre;
+    }
+    
+    /**
+    * Modifica el nombre del usuario
+    * @param nombre nombre del usuario
+    */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+    * Lee el apellido del usuario
+    * @return apellido del usuario
+    */
+    public String getApellido() {
+//        apellido = "Perez";
+        return apellido;
+    }
+
+    /**
+    * Modifica el apellido del usuario
+    * @param apellido apellido del usuario
+    */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+    * Lee el correo institucional del usuario
+    * @return correoinstitucional correo del usuario
+    */
+    public String getCorreoinstitucional() {
+//        correoinstitucional = "juanitoperez@uce.edu.ec";
+        return correoinstitucional;
+    }
+
+    /**
+    * Modifica el correo instirucional de usuario
+    * @param correoinstitucional correo del usuario
+    */
+    public void setCorreoinstitucional(String correoinstitucional) {
+        this.correoinstitucional = correoinstitucional;
+    }
+    
+    /**
+    * Lee el género musical del usuario
+    * @return generoMusical género musical del usuario
+    */
+    public String getGeneroMusical() {
+//        generoMusical = "Rock";
+        return generoMusical;
+    }
+
+    /**
+    * Modifica el género musical del usuario
+    * @param generoMusical género musical del usuario
+    */
+    public void setGeneroMusical(String generoMusical) {
+        this.generoMusical = generoMusical;
+    }
+    
+    /**
+    * Lee el color favorito del usuario
+    * @return colorFav color favorito del usuario
+    */
+    public String getColorFav() {
+//        colorFav = "Verde";
+        return colorFav;
+    }
+    
+    /**
+    * Modifica el color favorito del usuario
+    * @param colorFav color favorito del usuario
+    */
+    public void setColorFav(String colorFav) {
+        this.colorFav = colorFav;
+    }
+
+    /**
+    * Lee la comida favorita del usuario
+    * @return comidaFav comida favorita del usuario
+    */
+    public String getComidaFav() {
+//        comidaFav = "Papas";
+        return comidaFav;
+    }
+
+    /**
+    * Modifica el color favorito del usuario
+    * @param comidaFav comida favorita del usuario
+    */
+    public void setComidaFav(String comidaFav) {
+        this.comidaFav = comidaFav;
+    }
+
+    /**
+    * Lee el hobby del usuario
+    * @return hobby del usuario
+    */
+    public String getHobby() {
+//        hobby = "Correr";
+        return hobby;
+    }
+
+    /**
+    * Modifica el hobby del usuario
+    * @param hobby hobby del usuario
+    */
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    /**
+    * Lee el signo zodiacal del usuario
+    * @return signoZodiacal signoZodiacal del usuario
+    */
+    public String getSignoZodiacal() {
+//        signoZodiacal = "Cancer";
+        return signoZodiacal;
+    }
+
+    /**
+    * Modifica el signo zodiacal de usuario
+    * @param signoZodiacal signo zodiacal del usuario
+    */
+    public void setSignoZodiacal(String signoZodiacal) {
+        this.signoZodiacal = signoZodiacal;
+    }
+
+    /**
+    * Lee la película favorita del usuario
+    * @return peliculaFav pelicula favorita del usuario
+    */
+    public String getPeliculaFav() {
+//        peliculaFav = "Spider-Man";
+        return peliculaFav;
+    }
+
+    /**
+    * Modifica la película favotia del usuario
+    * @param peliculaFav pelicula favorita del usuario
+    */
+    public void setPeliculaFav(String peliculaFav) {
+        this.peliculaFav = peliculaFav;
+    }
+
+    /**
+    * Lee el libro favorito del usuario
+    * @return libroFav libro favorito del usuario
+    */
+    public String getLibroFav() {
+//        libroFav = "Condorito";
+        return libroFav;
+    }
+
+    /**
+    * Modifica el libro favorito de usuario
+    * @param libroFav libro favorito del usuario
+    */
+    public void setLibroFav(String libroFav) {
+        this.libroFav = libroFav;
+    }
+
+    /**
+    * Valida si es fumador social el usuario
+    * @return fumadorSocial fumador social del usuario
+    */
+    public boolean isFumadorSocial() {
+//        boolean result;
+        if (fumadorSocial == true) {
+            System.out.println("El " + getNombre() + " es fumador");
+        } else {
+            System.out.println("No es fumador " + getNombre());
+        }
+        return fumadorSocial;
+    }
+
+    /**
+    * Modifica el fumador social de usuario
+    * @param fumadorSocial fumador social del usuario
+    */
+    public void setFumadorSocial(boolean fumadorSocial) {
+        this.fumadorSocial = fumadorSocial;
+    }
+
+    /**
+    * Lee el animal favorito de usuario
+    * @return animalFav del usuario
+    */
+    public String getAnimalFav() {
+//        animalFav = "Puma";
+        return animalFav;
+    }
+
+    /**
+    * Modifica el animal favorito de usuario
+    * @param animalFav animal favorito del usuario
+    */
+    public void setAnimalFav(String animalFav) {
+        this.animalFav = animalFav;
+    }
+
+    /**
+    * Lee la edad del usuario
+    * @return edad del usuario
+    */
+    public int getEdad() {
+//        edad = 19;
+        return edad;
+    }
+
+    /**
+    * Modifica la edad del usuario
+    * @param edad del usuario
+    */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public Sugerencia[] getSugerencias() {
+        return sugerencias;
+    }
+    
+    /**
+     * Metodo Setter que sirve para modificar el valor
+     *
+     * @param clientes
+     */
+    public void setSugerencia(Sugerencia[] sugerencias) {
+        Usuario.sugerencias = sugerencias;
+    }
+    
+    /**
+    * Lee el número de sugerenicas del usuario
+    * @return numSugerencias nuúmero de sugerencias del usuario
+    */
+    public int getnumSugerencias() {
+        return numSugerencias;
+    }
+
+    /**
+    * Modifica el número de sugerencias del usuario
+    * @param numSugerencias número de sugerencias del usuario
+    */
+    public void setnumSugerencias(int numSugerencias) {
+        Usuario.numSugerencias = numSugerencias;
+    }
     
     
     
