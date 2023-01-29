282042922e4c754d2eaf70a5049cbf8299ea45d2
ronny <ronny@DESKTOP-R0RPKSA>
2023-01-29 12:55:43 -0500

    Métodos constructores

--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -12,7 +12,7 @@ public class Usuario {
     /**
     * Nombre del usuario
     */
-    String nombre;
+    private String nombre;
     /**
     * Apellido del usuario
     */
@@ -64,13 +64,99 @@ public class Usuario {
     /**
      * Compatibilidad del usuarii
      */
-    //private Oompatibilidad compatibilidad[];
+    private Compatibilidad compatibilidad;
     /**
      * Arreglo de las sugerencias creadas
      */
-    //private static Sugerencia sugerencias[];
+    private static Sugerencia sugerencias[];
     /**
      * Número de sugerencias
      */
     private static int numSugerencias;
+    /**
+     * Arreglo de los usuarios creados
+     */
+    private static Usuario Usuarios[];
+    /**
+     * Número de los usuarios
+     */
+    private static int numUsuarios;
+    
+    /**
+     * Bloque estático
+     */
+    static {
+        sugerencias = new Sugerencia[3];
+        numSugerencias = 0;
+        Usuarios = new Usuario[2];
+        numUsuarios = 0; 
+    }
+    
+    /**
+     * Método constructor sin argumentos
+     */
+    public Usuario(){        
+    }
+     
+    /**
+     * Método constructor con argumentos
+     * 
+     * @param nombre
+     * @param apellido
+     * @param edad
+     * @param correoInstitucional
+     * @param generoMusical
+     * @param colorFav
+     * @param comidaFav
+     * @param libroFav
+     * @param animalFav
+     * @param peliculaFav
+     * @param hobby
+     * @param signoZodiacal
+     * @param fumadorSocial 
+     */
+    public Usuario(String nombre, String apellido, int edad, String correoInstitucional, String generoMusical, String colorFav,
+                    String comidaFav, String libroFav, String animalFav, String peliculaFav, String hobby, String signoZodiacal,
+                    boolean fumadorSocial){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correoinstitucional = correoInstitucional;
+        this.generoMusical = generoMusical;
+        this.colorFav = colorFav;
+        this.comidaFav = comidaFav;
+        this.libroFav = libroFav;
+        this.animalFav = animalFav;
+        this.peliculaFav = peliculaFav;
+        this.hobby = hobby;
+        this.signoZodiacal = signoZodiacal;
+        this.fumadorSocial= fumadorSocial;
+    }
+    
+    public Usuario(Usuario user1){
+        //nombre = user1.getNombre();
+    }
+    
+    /**
+     * Métodos Set
+     */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
