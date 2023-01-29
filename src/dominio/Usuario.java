449b96d21b01fdf9e6e529a9b2b1018482eab048
ronny <ronny@DESKTOP-R0RPKSA>
2023-01-29 11:54:57 -0500

    Creación de los atributos de la case usuario

--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -7,4 +7,70 @@ package dominio;
  */
 public class Usuario {
 
+    //ATRIBUTOS
+    
+    /**
+    * Nombre del usuario
+    */
+    String nombre;
+    /**
+    * Apellido del usuario
+    */
+    private String apellido;
+    /**
+    * Edad del usuario
+    */
+    private int edad;
+    /**
+    * Correo institucional del usuario
+    */
+    private String correoinstitucional;
+    /**
+    * Género musical del usuario
+    */
+    private String generoMusical;
+    /**
+    * Color favorito del usuario
+    */
+    private String colorFav;
+    /**
+    * Comida favorita del usuario
+    */
+    private String comidaFav;
+    /**
+    * Libro favorito del usuario
+    */
+    private String libroFav;
+    /**
+    * animal favorito del usuario
+    */
+    private String animalFav;
+    /**
+    * Película favorita del usuario
+    */
+    private String peliculaFav;
+    /**
+    * Hobby del usuario
+    */
+    private String hobby;
+    /**
+    * Signo zodiacal del usuario
+    */
+    private String signoZodiacal;
+    /**
+    * Fumador Social del usuario
+    */
+    private boolean fumadorSocial;
+    /**
+     * Compatibilidad del usuarii
+     */
+    private Oompatibilidad compatibilidad[];
+    /**
+     * Arreglo de las sugerencias creadas
+     */
+    private static Sugerencia sugerencias[];
+    /**
+     * Número de sugerencias
+     */
+    private static int numSugerencias;
 }
