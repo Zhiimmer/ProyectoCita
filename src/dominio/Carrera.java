88aa313be247065712744da58a60219dc3cb245e
Zhiimmer <mauricio.550.1995@gmail.com>
2023-01-31 18:40:53 -0500

    Final en la clase Carrera

--- src/dominio/Carrera.java
+++ src/dominio/Carrera.java
@@ -6,8 +6,17 @@ package dominio;
  */
 public class Carrera {
 
+    //Atributos
     String nombreCarrera;
     private String semestre;
+    private final String nomCarrera = null;
+
+    /**
+     * Método constructor sin argumentos con la "sobre carga de métodos"
+     */
+    public Carrera() {
+        this("Sistemas", "segundo", "Civil");
+    }
 
     /**
      * Método constructor con argumentos
@@ -15,40 +24,36 @@ public class Carrera {
      * @param nombreCarrera
      * @param semestre
      */
-    public Carrera(String nombreCarrera, String semestre) {
+    public Carrera(String nombreCarrera, String semestre, String nomCarrera) {
         this.nombreCarrera = nombreCarrera;
         this.semestre = semestre;
     }
+
     /**
-     * Metodo de enumerar 
+     * Metodo de enumerar
      */
     public enum Carreras {
-    SISTEMASINFORMACION,
-    CIVIL,
-    DISEÑOINDUSTRIAL, 
-    QUIMICA,
-    MINAS,
-    PETROLEOS,
-    AMBIENTAL,
-    COMPUTACION, 
-    GEOLOGIA
-}
-    
-    /**
-     * Método constructor sin argumentos con la "sobre carga de métodos"
-     */
-    public Carrera() {
-        this("Sistemas", "segundo");
+        SISTEMASINFORMACION,
+        CIVIL,
+        DISEÑOINDUSTRIAL,
+        QUIMICA,
+        MINAS,
+        PETROLEOS,
+        AMBIENTAL,
+        COMPUTACION,
+        GEOLOGIA
     }
-
+   
     /**
      ** Método constructor referenciado a un objeto con la "sobre carga de
      * métodos"
+     *
      * @param carre1
      */
-    public Carrera (Carrera carre1){
-        this (carre1.getNombreCarrera(), carre1.getSemestre());
+    public Carrera(Carrera carre1) {
+        this(carre1.getNombreCarrera(), carre1.getSemestre(), carre1.getNomCarrera());
     }
+
     /**
      * Método set y get
      *
@@ -70,6 +75,10 @@ public class Carrera {
         this.semestre = semestre;
     }
 
+    public String getNomCarrera() {
+        return nomCarrera;
+    }
+   
     //Metodo toString
     /**
      * Este es un metódo que nos permite mostrar la información completa del
