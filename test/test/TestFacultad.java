c4a3d0e8a908228d3d0aa7449444b21c0d865ba8
ronny <ronny@DESKTOP-R0RPKSA>
2023-02-13 17:49:22 -0500

    Arreglar el test Facultad, y esta bien el Iadministrar

--- test/test/TestFacultad.java
+++ test/test/TestFacultad.java
@@ -4,6 +4,7 @@
  */
 package test;
 
+import dominio.Carrera;
 import dominio.Facultad;
 
 /**
@@ -21,7 +22,7 @@ public class TestFacultad {
         
         //Listar los carreras
         System.out.println("Se van a imprimir la lista de las carreras de las citas");
-        System.out.println(carrera.listarCarrera());
+        System.out.println(carrera.listar(carrera));
         
         //Buscar los carreras
         System.out.println("Buscamos el lugar llamado Ing. quimica: \n\n" + carrera.buscarCarrera(0));
@@ -29,12 +30,12 @@ public class TestFacultad {
         //Editar los carreras
         System.out.println("Se va a editar el lugar Ing. Sistemas por Ing Civil");
 //        carrera.editarCarrera(1, "Ing Civil");
-        System.out.println(carrera.listarCarrera());
+        System.out.println(carrera.listar(carrera));
         
         //Eliminar carreras
         System.out.println("Vamos a eliminar Ing. Sistemas de las carreras de la cita");
-        carrera.eliminarCarrera(1,"Ing.Sistemas");
-        System.out.println(carrera.listarCarrera());
+       // carrera.borrar(carrera, 2);
+        System.out.println(carrera.listar(carrera));
 
     }
 }
