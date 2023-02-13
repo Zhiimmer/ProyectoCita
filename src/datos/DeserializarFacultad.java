974d7c920c2ef66c7cf480f9d2a383982071a73d
Gaby Fernandez <Gaby Fernandez@DESKTOP-Q7EAC0I.local>
2023-02-12 20:16:16 -0500

    agregar el serializar y deserializar en facultad

--- src/datos/DeserializarFacultad.java
+++ src/datos/DeserializarFacultad.java
@@ -5,37 +5,41 @@
 package datos;
 
 import dominio.Facultad;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Gaby Fernandez
  */
-public class SerializarFacultad {
+public class DeserializarFacultad {
 
-    public SerializarFacultad() {
-        Facultad fa = new Facultad();
-
+    public DeserializarFacultad() {
+        Facultad fa = null;
         try {
+            FileInputStream f = new FileInputStream("facultad.txt");
 
-            FileOutputStream f = new FileOutputStream("facultad.txt");
+            ObjectInputStream o = new ObjectInputStream(f);
 
-            ObjectOutputStream o = new ObjectOutputStream(f);
-
-            o.writeObject(f);
+            fa = (Facultad) o.readObject();
             o.close();
-
+            System.out.println("Facultad desearilizar" + fa);
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
+
         } catch (IOException ex) {
             ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+           ex.printStackTrace();
         }
-    }
 
+    }
+    
     public static void main(String[] args) {
-        new SerializarFacultad();
+        new DeserializarFacultad();
     }
 }
