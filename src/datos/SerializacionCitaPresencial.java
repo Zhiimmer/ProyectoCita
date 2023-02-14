a80090fc6d0fdb3b314b50cbd6201bd5abf9a892
ronny <ronny@DESKTOP-R0RPKSA>
2023-02-14 15:13:07 -0500

    Serializacion citap

--- src/datos/SerializacionCitaPresencial.java
+++ src/datos/SerializacionCitaPresencial.java
@@ -4,6 +4,7 @@
  */
 package datos;
 
+import dominio.CitaPresencial;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -16,26 +17,26 @@ import java.util.Date;
  */
 public class SerializacionCitaPresencial {
     
-//    public static void main(String[] args ){
-//            
-//            Directiva[] d = new Directiva[3];
-//            d[0] = new Directiva(007, new Date(2021, 5, 13), new Date(2022, 5, 13), "Jefferson Norton", "Alejandro Cueva");
-//            d[1] = new Directiva(006, new Date(2022, 5, 13), new Date(2023, 5, 13), "   Luis Guzman", "Alexandra Ortiz");
-//            d[2] = new Directiva(005, new Date(2023, 5, 13), new Date(2024, 5, 13), "    Mia Colucci", "Roberta Pardo");
-//        try {
-//            ObjectOutputStream escribir = new ObjectOutputStream(new FileOutputStream("directiva.txt"));
-//            escribir.writeObject(d);
-//            escribir.close();
-//            
-//            ObjectInputStream recuperar = new ObjectInputStream(new FileInputStream("directiva.txt"));
-//            Directiva [] ingresoRecuperado = (Directiva[]) recuperar.readObject();
-//            recuperar.close();
-//            for (Directiva directiva: ingresoRecuperado) {
-//                System.out.println(directiva);
-//            }
-//        }catch(Exception e){
-//            
-//        }
-//        }
-//    
+    public static void main(String[] args ){
+            
+            CitaPresencial[] citas = new CitaPresencial[3];
+            citas[0] = new CitaPresencial("Mateo", new Date(2022, 5, 13), "16:30", "18:30");
+            citas[1] = new CitaPresencial("Luis", new Date(2022, 5, 13), "13:00", "14:00");
+            citas[2] = new CitaPresencial("Jorge", new Date(2022, 5, 13), "14:15", "16:00");
+        try {
+            ObjectOutputStream escribir = new ObjectOutputStream(new FileOutputStream("citaPresencial.txt"));
+            escribir.writeObject(citas);
+            escribir.close();
+            
+            ObjectInputStream recuperar = new ObjectInputStream(new FileInputStream("citaPresencial.txt"));
+            CitaPresencial [] ingresoRecuperado = (CitaPresencial[]) recuperar.readObject();
+            recuperar.close();
+            for (CitaPresencial citap: ingresoRecuperado) {
+                System.out.println(citap);
+            }
+        }catch(Exception e){
+            
+        }
+        }
+    
 }
